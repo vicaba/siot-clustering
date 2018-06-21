f30d55aaba2ea51d38ada1b344bf8b4491a909b5
Victor Caballero <victor.caballero@salle.url.edu>
2018-06-21 11:52:23 +0200

    memory added as a parameter for rescheduleCluster

--- src/main/scala/cluster/scheduler/ClusterRescheduler.scala
+++ src/main/scala/cluster/scheduler/ClusterRescheduler.scala
@@ -13,29 +13,38 @@ object ClusterRescheduler {
 
   class PointChanged(val point: Point, val change: MatrixResult[Double])
 
-  def rescheduleCluster(cluster: Cluster, metric: Metric, improvement: Double): (Cluster, List[PointChanged]) = {
+  /**
+   * Reschedules the points inside the cluster in order to minimize the metric function.
+   *
+   * @param cluster The cluster to reschedule
+   * @param metric The metric function
+   * @param improvement Fraction of unity. Stop when this improvement is reached
+   * @param memory When the cluster metric doesn't change after memory reschedules, stop.
+   * @return The rescheduled cluster.
+   */
+  def rescheduleCluster(cluster: Cluster, metric: Metric, improvement: Double, memory: Int = 2):
+  (Cluster, List[PointChanged]) = {
 
     val initialMetric = metric(cluster)
 
     def improvementPercentage(improvement: Double): Double = 1 - improvement / initialMetric
 
-
     @tailrec
-    def reschedule(improvementAchieved: Double, memory: Memory[Double], cluster: Cluster, changes: List[PointChanged]):
+    def reschedule(currentClusterMetric: Double, memory: Memory[Double], cluster: Cluster, changes: List[PointChanged]):
     (Cluster, List[PointChanged]) = {
 
-      val improvedEnough = improvementPercentage(improvementAchieved) > improvement
+      val improvedEnough = improvementPercentage(currentClusterMetric) > improvement
 
       if (improvedEnough || memory.areAllElementsEqual()) (cluster, changes)
       else {
         val pointChange = rescheduleOnePoint(cluster, metric)
         val pointChanged = new PointChanged(pointChange.point, pointChange.change)
-        val thisImprovement = metric(pointChange.cluster)
-        reschedule(thisImprovement, thisImprovement +: memory, pointChange.cluster, pointChanged +: changes)
+        val _currentMetric = metric(pointChange.cluster)
+        reschedule(_currentMetric, _currentMetric +: memory, pointChange.cluster, pointChanged +: changes)
       }
     }
 
-    reschedule(0, Memory(10), cluster, List.empty)
+    reschedule(0, Memory(memory), cluster, List.empty)
 
   }
 
