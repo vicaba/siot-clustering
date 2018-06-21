97e02c734c9554fa01a181818f06eebbae3b33fb
Victor Caballero <victor.caballero@salle.url.edu>
2018-06-21 11:32:06 +0200

    Tests refactored

--- src/test/scala/cluster/scheduler/ClusterReschedulerSpec.scala
+++ src/test/scala/cluster/scheduler/ClusterReschedulerSpec.scala
@@ -10,21 +10,26 @@ class ClusterReschedulerSpec extends FeatureSpec with GivenWhenThen {
 
   val metric = Metric.maxMin
 
+  val globalPoints = List(
+    DenseMatrix((0.0, 3.0, 3.0, 0.0), (0.0, 4.0, 4.0, 0.0))
+    , DenseMatrix((0.0, 0.0, 3.0, 3.0), (0.0, 4.0, 4.0, 0.0))
+  ).zipWithIndex.map { case (m, idx) =>
+    Point(idx, m, Some(0))
+  }.toSet
+
+  val globalCluster = Cluster(0, "0", globalPoints)
+
+
   feature("Vector cluster.scheduler.ClusterRescheduler") {
     scenario("schedules point best that minimizes overall distanceFunction") {
 
       Given("A cluster with two points in it")
 
-      val points = List(
-        DenseMatrix((0.0, 3.0, 3.0, 0.0), (0.0, 4.0, 4.0, 0.0))
-        , DenseMatrix((0.0, 0.0, 3.0, 3.0), (0.0, 4.0, 4.0, 0.0))
-      ).zipWithIndex.map { case (m, idx) =>
-        Point(idx, m, Some(0))
-      }.toSet
+      val points = globalPoints
 
       And("a cluster with those points")
 
-      val cluster = Cluster(0, "0", points)
+      val cluster = globalCluster
 
       When("asked to reschedule one point")
 
@@ -41,22 +46,16 @@ class ClusterReschedulerSpec extends FeatureSpec with GivenWhenThen {
     scenario("schedules cluster best that minimizes overall distanceFunction") {
       Given("A cluster with two points in it")
 
-      val points = List(
-        DenseMatrix((0.0, 3.0, 3.0, 0.0), (0.0, 4.0, 4.0, 0.0))
-        , DenseMatrix((0.0, 0.0, 3.0, 3.0), (0.0, 4.0, 4.0, 0.0))
-      ).zipWithIndex.map { case (m, idx) =>
-        Point(idx, m, Some(0))
-      }.toSet
+      val points = globalPoints
 
       And("a cluster with those points")
 
-      val cluster = Cluster(0, "0", points)
+      val cluster = globalCluster
 
       And("a vector solution")
       val vector = DenseVector(7.0, 7.0, 7.0, 7.0)
 
-
-      When("asked to reschedule one point")
+      When("asked to reschedule the cluster")
 
       val (betterCluster, scheduleResult) = ClusterRescheduler.rescheduleCluster(cluster, metric, 1)
 
@@ -66,7 +65,7 @@ class ClusterReschedulerSpec extends FeatureSpec with GivenWhenThen {
 
       betterCompatibility should be < originalCompatibility
 
-      And("the syntheticValue should be equal to the only possible solution")
+      And("the syntheticCenter should be equal to the only possible solution")
       betterCluster.syntheticCenter shouldEqual vector
     }
   }
