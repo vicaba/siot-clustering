d56994414ef0a4a0d1bbd7eeed496b0fc5a72200
Victor Caballero <victor.caballero@salle.url.edu>
2018-09-20 16:42:26 +0200

    Functions improved and tests created

--- src/main/scala/algorithm2/Ops.scala
+++ src/main/scala/algorithm2/Ops.scala
@@ -6,20 +6,54 @@ import types.Point._
 
 object Ops {
 
-  def findMirror(origin: Point, center: Point): Vector[Double] = {
-    val diff = center.syntheticValue - origin.syntheticValue
-    center.syntheticValue + diff
-  }
+  /**
+    * Given two points A and B, finds C as the mirror image of point A reflected into the plane perpendicular to line AB
+    * and situated at distance norm(AB, 2) from A
+    *
+    * @param a the point to find the mirror image of
+    * @param c the point where a perpendicular plane to line AB passes through
+    * @return the mirror image
+    */
+  def findMirror(a: Vector[Double], c: Vector[Double]): Vector[Double] = (2.0 * c) - a
 
-  def findClosestMirror(origin: Point,
-                        center: Point,
-                        points: Seq[Point]): Seq[(Double, Point)] = {
+  /**
+    * Given a set of points, finds the closest point to the mirror image of point A. See [[findClosestMirror()]].
+    *
+    * @param origin the point to find the mirror image of
+    * @param center the point where a perpendicular plane to line AB passes through, acting as the mirror
+    * @param points the set of points to find the closest to the image of point A
+    * @return the closest mirror image
+    */
+  def findClosestMirror(origin: Point, center: Point, points: Seq[Point]): Seq[(Double, Point)] = {
 
     val idealMirror = findMirror(origin, center)
 
-    points.zipWithIndex.map { case (point, idx) =>
-      (norm(idealMirror - point.syntheticValue, 2), point)
-    }.sortBy(_._1)
+    points.zipWithIndex
+      .map {
+        case (point, idx) =>
+          (norm(idealMirror - point.syntheticValue, 2), point)
+      }
+      .sortBy(_._1)
+  }
+
+  /**
+    * Given a set of points, finds the closest point to the mirror image of point A. See [[findClosestMirror()]].
+    *
+    * @param origin the point to find the mirror image of
+    * @param center the point where a perpendicular plane to line AB passes through, acting as the mirror
+    * @param points the set of points to find the closest to the image of point A
+    * @return the closest mirror image
+    */
+  def findClosestMirror(origin: Vector[Double], center: Vector[Double], points: Seq[Vector[Double]]): Seq[(Double, Vector[Double])] = {
+
+    val idealMirror = findMirror(origin, center)
+
+    points.zipWithIndex
+      .map {
+        case (point, idx) =>
+          (norm(idealMirror - point, 2), point)
+      }
+      .sortBy(_._1)
   }
 
   def main(args: Array[String]): Unit = {
@@ -33,9 +67,24 @@ object Ops {
         Point(idx, m, None)(Types2)
     }.toVector
 
-    assert(points(2).syntheticValue == findMirror(points(0), points(1)))
+    val pointsWithoutExactMirror = List(
+      DenseMatrix((5.0, 7.0)),
+      DenseMatrix((7.0, 10.0)),
+      DenseMatrix((10.0, 13.0))
+    ).zipWithIndex.map {
+      case (m, idx) =>
+        Point(idx, m, None)(Types2)
+    }.toVector
 
-    assert(points(2).syntheticValue == findClosestMirror(points(0), points(1), points).head._2.syntheticValue)
+    assert(Vector(9.0, 13.0) == findMirror(points(0), points(1)))
+
+    val cm = findClosestMirror(points(0), points(1), pointsWithoutExactMirror)
+
+    assert(pointsWithoutExactMirror(2).syntheticValue == findClosestMirror(
+      points(0),
+      points(1),
+      pointsWithoutExactMirror).head._2.syntheticValue)
+
   }
 
 }
---
+++ src/test/scala/algorithm2/OpsSpec.scala
@@ -0,0 +1,58 @@
+package algorithm2
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import org.scalatest.{FeatureSpec, GivenWhenThen}
+import types.{Point, Types2, TypesT}
+import algorithm2.Ops._
+import org.scalatest.Matchers._
+
+class OpsSpec extends FeatureSpec with GivenWhenThen {
+
+  implicit val types: TypesT = Types2
+
+  def createPoint(v: Vector[Double]): Point = {
+    Point(0, DenseMatrix(v), None)
+  }
+
+  feature("algorithm2.Ops.findMirror") {
+    scenario("Given a point finds the mirror image of it") {
+
+      Given("A point")
+      val p = createPoint(Vector(5.0, 7.0))
+
+      And("A center")
+      val c = createPoint(Vector(7.0, 10.0))
+
+      When("finding the mirror image")
+      val result = findMirror(p, c)
+
+      Then("the result is the expected")
+      result shouldBe DenseVector(9.0, 13.0)
+    }
+
+    scenario("Given a point finds the closest mirror image from a set of points") {
+
+      val pointsWithoutExactMirror = List(
+        DenseMatrix((5.0, 7.0)),
+        DenseMatrix((7.0, 10.0)),
+        DenseMatrix((10.0, 13.0)),
+      ).zipWithIndex.map {
+        case (m, idx) =>
+          Point(idx, m, None)(Types2)
+      }.toVector
+
+      Given("A point")
+      val p = pointsWithoutExactMirror(0)
+
+      And("A center")
+      val c = pointsWithoutExactMirror(1)
+
+      When("finding the closest mirror image")
+      val result = findClosestMirror(p, c, pointsWithoutExactMirror)
+
+      Then("the result is the expected")
+      result.head._2 shouldBe pointsWithoutExactMirror(2)
+    }
+
+  }
+}
