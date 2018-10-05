7660fff073a7837b79b4e3bb7663c0ba8f89b861
Victor Caballero <victor.caballero@salle.url.edu>
2018-10-05 15:33:49 +0200

    Creating the factorizer to see in which order clusters of K should be created

---
+++ src/main/scala/utils/MathUtils.scala
@@ -0,0 +1,24 @@
+package utils
+import scala.annotation.tailrec
+
+object MathUtils {
+
+  def factorize(x: Int): List[Int] = {
+    @tailrec
+    def _factorize(x: Int, a: Int = 2, list: List[Int] = Nil): List[Int] = a * a > x match {
+      case false if x % a == 0 => _factorize(x / a, a, a :: list)
+      case false => _factorize(x, a + 1, list)
+      case true  => x :: list
+    }
+    _factorize(x)
+  }
+
+  def clusteringOrder(points: Int, k: Int): List[Int] = {
+    val pointsFactors = factorize(points)
+    val kFactors = factorize(k)
+    val groupedPointsFactors = pointsFactors.groupBy(_)
+    val groupedKFactors = kFactors.groupBy(_)
+
+  }
+
+}
