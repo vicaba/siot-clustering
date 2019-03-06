3e47178a3d9c4c822fa68867c047745afbeffcec
Victor Caballero <victor.caballero@salle.url.edu>
2019-03-06 12:18:35 +0100

    MirrorImage refactored and commented

--- src/main/scala/types/ops/MirrorImage.scala
+++ src/main/scala/types/ops/MirrorImage.scala
@@ -7,20 +7,55 @@ import types.immutable.Point
 
 object MirrorImage {
 
+  /**
+    * Typeclass that provides the mirror for type T
+    *
+    * @tparam T
+    */
   trait FindMirror[T] {
 
+    /**
+      * Given an origin and a center, find the mirror image of origin from that center
+      *
+      * @param origin
+      * @param center
+      * @return
+      */
     implicit def apply(origin: T, center: SyntheticDataType): SyntheticDataType
 
   }
 
+  /**
+    * Typeclass that provides distance between types T
+    *
+    * @tparam T
+    */
   trait DistanceFunc[T] {
 
+    /**
+      * Calculates distance between e1 and e2
+      *
+      * @param e1
+      * @param e2
+      * @return the distance between e1 and e2
+      */
     implicit def apply(e1: T, e2: T): Double
 
+    /**
+      * Calcuates distance between e1 and e2
+      *
+      * @param e1
+      * @param e2
+      * @return the distance between e1 and e2
+      */
     implicit def apply(e1: T, e2: SyntheticDataType): Double
 
   }
 
+  /**
+  * Typeclass that provides distance and mirror typeclasses
+    * @tparam T
+    */
   trait Mirrored[T] {
 
     implicit def findMirror: FindMirror[T]
@@ -41,10 +76,8 @@ object MirrorImage {
   }
 
   implicit object MirroredType extends Mirrored[Type] {
-    override def findMirror: FindMirror[Type] = new FindMirror[Type] {
-      override def apply(origin: Type, center: SyntheticDataType): SyntheticDataType =
-        (2.0 * center) - origin.syntheticValue
-    }
+    override def findMirror: FindMirror[Type] =
+      (origin: Type, center: SyntheticDataType) => (2.0 * center) - origin.syntheticValue
 
     override implicit def distance: DistanceFunc[Type] = new DistanceFunc[Type] {
       override def apply(e1: Type, e2: Type): Double              = norm(e2.syntheticValue - e1.syntheticValue, 2)
@@ -54,10 +87,8 @@ object MirrorImage {
   }
 
   implicit object MirroredCluster extends Mirrored[Cluster] {
-    override def findMirror: FindMirror[Cluster] = new FindMirror[Cluster] {
-      override def apply(origin: Cluster, center: SyntheticDataType): SyntheticDataType =
-        (2.0 * center) - origin.syntheticValue
-    }
+    override def findMirror: FindMirror[Cluster] =
+      (origin: Cluster, center: SyntheticDataType) => (2.0 * center) - origin.syntheticValue
 
     override implicit def distance: DistanceFunc[Cluster] = new DistanceFunc[Cluster] {
       override def apply(e1: Cluster, e2: Cluster): Double           = norm(e2.syntheticValue - e1.syntheticValue, 2)
@@ -67,10 +98,8 @@ object MirrorImage {
   }
 
   implicit object MirroredPoint extends Mirrored[Point] {
-    override def findMirror: FindMirror[Point] = new FindMirror[Point] {
-      override def apply(origin: Point, center: SyntheticDataType): SyntheticDataType =
-        (2.0 * center) - origin.syntheticValue
-    }
+    override def findMirror: FindMirror[Point] =
+      (origin: Point, center: SyntheticDataType) => (2.0 * center) - origin.syntheticValue
 
     override implicit def distance: DistanceFunc[Point] = new DistanceFunc[Point] {
       override def apply(e1: Point, e2: Point): Double             = norm(e2.centroid - e1.centroid, 2)
@@ -90,6 +119,16 @@ object MirrorImage {
   def findMirror[T: Mirrored](a: T, c: SyntheticDataType): SyntheticDataType =
     implicitly[Mirrored[T]].findMirror(a, c)
 
+  /**
+    * Given two points origin and center, orders points from the the closest mirror image of point origin
+    * reflected into the plane perpendicular to line origin-center and situated at distance norm(origin-center, 2)
+    * from origin
+    * @param origin the point to find the mirror image of
+    * @param center the point where a perpendicular plane to line AB passes through
+    * @param points the set of points to search for the closest mirror image
+    * @tparam T the type that provides a typeclass to find the mirror image from it
+    * @return A list of distances and points ordered by its closeness in incremental order (shortest distance first)
+    */
   def findClosestMirrors[T: Mirrored](origin: T,
                                       center: SyntheticDataType,
                                       points: IndexedSeq[T]): IndexedSeq[(Double, T)] = {
@@ -106,6 +145,9 @@ object MirrorImage {
       .sortBy(_._1)
   }
 
+  /**
+  * The same as [[findClosestMirrors()]] but only returns the closest
+    */
   def findClosestMirror[T: Mirrored](origin: T, center: SyntheticDataType, points: IndexedSeq[T]): Option[T] =
     findClosestMirrors(origin, center, points).headOption.map(_._2)
 
