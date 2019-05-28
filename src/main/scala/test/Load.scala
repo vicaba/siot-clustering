14ce6665ea42870372d3d5767cfc83cc66d8dafc
Victor Caballero <victor.caballero@salle.url.edu>
2019-05-28 16:44:06 +0200

    Backup cimmit

--- src/main/scala/test/Load.scala
+++ src/main/scala/test/Load.scala
@@ -10,6 +10,15 @@ import scala.language.higherKinds
 
 sealed trait Load {
 
+  def id: Int
+
+  override def equals(obj: Any): Boolean = obj match {
+    case s: Load => s.id == this.id && this.getClass == s.getClass
+    case _       => false
+  }
+
+  override def hashCode(): Int = this.id
+
   /**
     * Indicates the slot time when this load starts
     */
@@ -17,48 +26,24 @@ sealed trait Load {
   def totalEnergy: Double
 }
 
-sealed trait IdLoad extends Load {
+sealed trait SingleLoad extends Load
 
-  def id: Int
+sealed trait AccumulatedLoad extends Load
 
-  override def equals(obj: Any): Boolean = obj match {
-    case s: IdLoad => s.id == this.id
-    case _         => false
-  }
+sealed trait OneSlotLoad extends Load
 
-  override def hashCode(): Int = this.id
-
-}
-
-sealed trait SingleLoad extends IdLoad
-
-sealed trait AccumulatedLoad extends IdLoad
-
-sealed trait OneSlotLoad extends IdLoad
-
-sealed trait SpanSlotLoad extends IdLoad {
+sealed trait SpanSlotLoad extends Load {
   def span: Int
   def amplitudePerSlot: Vector[Double]
 }
 
-case class FixedLoad(override val id: Int, override val positionInT: Int, override val totalEnergy: Double)
-    extends OneSlotLoad
+case class SpanSlotFixedLoad(override val id: Int,
+                             override val positionInT: Int,
+                             override val amplitudePerSlot: Vector[Double])
+    extends SpanSlotLoad
     with SingleLoad {
-  override def toString: String = s"Fi($id, $positionInT, $totalEnergy)"
-}
-
-case class OneSlotFlexibleLoad(override val id: Int, override val positionInT: Int, override val totalEnergy: Double)
-    extends OneSlotLoad
-    with SingleLoad {
-  override def toString: String = s"Fl($id, $positionInT, $totalEnergy)"
-}
-
-case class OneSlotAccumulatedLoad(override val positionInT: Int, loads: List[OneSlotLoad])
-    extends OneSlotLoad
-    with AccumulatedLoad {
-  override val id: Int          = positionInT
-  def totalEnergy: Double       = loads.foldLeft(0.0)((accum, load) => accum + load.totalEnergy)
-  override def toString: String = s"Acc($positionInT, $totalEnergy -> $loads)"
+  override def span: Int           = amplitudePerSlot.size
+  override def totalEnergy: Double = amplitudePerSlot.foldLeft(0.0)((accum, a) => accum + a)
 }
 
 case class SpanSlotFlexibleLoad(override val id: Int,
@@ -70,59 +55,47 @@ case class SpanSlotFlexibleLoad(override val id: Int,
   override def totalEnergy: Double = amplitudePerSlot.foldLeft(0.0)((accum, a) => accum + a)
 }
 
-case class SpanSlotAccumulatedLoad(override val positionInT: Int,
-                                   private val _fixedLoads: List[FixedLoad],
-                                   loads: Set[SpanSlotFlexibleLoad])
+case class SpanSlotAccumulatedLoad(override val positionInT: Int, private val _loads: Set[SpanSlotLoad])
     extends SpanSlotLoad
     with AccumulatedLoad {
 
+  val loads: Set[SpanSlotLoad] = _loads.filter(_.isInstanceOf[SpanSlotFlexibleLoad])
+
+  val fixedLoads: Set[SpanSlotLoad] = _loads.filter(_.isInstanceOf[SpanSlotFixedLoad])
+
   override val id: Int = positionInT
 
-  override val span: Int = if (_fixedLoads.nonEmpty) _fixedLoads.size else loads.map(l => l.span + l.positionInT).max
-
-  val fixedLoads: List[FixedLoad] =
-    if (_fixedLoads.nonEmpty) _fixedLoads else Load.toFixedLoads(for (_ <- positionInT until span) yield 0.0).toList
+  override val span: Int = loads.map(l => l.span + l.positionInT).max
 
   override val amplitudePerSlot: Vector[Double] =
     SeqOps.sum(
-      fixedLoads.map(_.totalEnergy).toVector :: loads
-        .map(expandSpanSlotFlexibleLoadToVector(_, span))
-        .toList)
+      loads
+        .map(expandSpanSlotLoadToVector(_, span))
+        .toList
+    )
 
   def totalEnergy: Double =
     (fixedLoads.map(_.totalEnergy) ++: loads.map(_.totalEnergy)).foldLeft(0.0)((accum, l) => accum + l)
 
   override def toString: String = s"Acc($positionInT, $totalEnergy -> $loads)"
 
-  private def expandSpanSlotFlexibleLoadToVector(load: SpanSlotFlexibleLoad, vectorSize: Int): Vector[Double] =
-    (
+  private def expandSpanSlotLoadToVector(load: SpanSlotLoad, vectorSize: Int): Vector[Double] = {
+    val res = (
       (for (_ <- 0 until load.positionInT) yield 0.0) ++
         load.amplitudePerSlot ++
-        (for (_ <- (load.positionInT + load.span) until span) yield 0.0)
+        (for (_ <- (load.positionInT + load.span) until vectorSize) yield 0.0)
     ).toVector
+
+    res
+  }
 
 }
 
-class Loads(val fixedLoads: Vector[FixedLoad], val flexibleLoads: Vector[OneSlotFlexibleLoad])
-
 object Load {
 
-  def toFixedLoads[S[X] <: Seq[X]](s: S[Double])(
-      implicit cbf: CanBuildFrom[Nothing, FixedLoad, S[FixedLoad]]): S[FixedLoad] = {
-    s.zipWithIndex.map { case (e, idx) => FixedLoad(idx, idx, e) }.to[S]
+  def toSpanSlotFixedLoad(s: Seq[Double]): SpanSlotFixedLoad = {
+    SpanSlotFixedLoad(0, 0, s.toVector)
   }
-
-  def toFlexibleLoads[S[X] <: Seq[X]](s: S[Double])(
-      implicit cbf: CanBuildFrom[Nothing, OneSlotFlexibleLoad, S[OneSlotFlexibleLoad]]): S[OneSlotFlexibleLoad] = {
-    s.zipWithIndex.map { case (e, idx) => OneSlotFlexibleLoad(idx, idx, e) }.to[S]
-  }
-
-  def flatten(s: Seq[Load]): Vector[IdLoad] =
-    s.flatMap {
-      case al: OneSlotAccumulatedLoad => al.loads
-      case s: IdLoad                  => Vector(s)
-      case _                          => Vector()
-    }.toVector
 
   class LoadOrdering extends Ordering[Load] {
     override def compare(x: Load, y: Load): Int = implicitly[Ordering[Double]].compare(x.totalEnergy, y.totalEnergy)
--- src/test/scala/test/SpanSlotAccumulatedLoadSpec.scala
+++ src/test/scala/test/SpanSlotAccumulatedLoadSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.Matchers._
 
 class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
-  feature("SpanSlotAccumulatedLoad accumulates fixed loads") {
+/*  feature("SpanSlotAccumulatedLoad accumulates fixed loads") {
 
     scenario("SpanSlotAccumulatedLoad accumulates one FixedLoad") {
 
@@ -16,7 +16,7 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
       val fixedLoad = toFixedLoads(rawFixedLoads)
 
-      When("creating a SpanSlotAccumulatedLoad")
+      When("a SpanSlotAccumulatedLoad is created")
 
       val spanSlotAccumulatedLoad = SpanSlotAccumulatedLoad(0, fixedLoad.toList, Set.empty)
 
@@ -38,7 +38,7 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
       val fixedLoad = toFixedLoads(rawFixedLoads)
 
-      When("creating a SpanSlotAccumulatedLoad")
+      When("a SpanSlotAccumulatedLoad is created")
 
       val spanSlotAccumulatedLoad = SpanSlotAccumulatedLoad(0, fixedLoad.toList, Set.empty)
 
@@ -52,11 +52,11 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
     }
 
-  }
+  }*/
 
   feature("SpanSlotAccumulatedLoad accumulates flexible loads") {
 
-    scenario("SpanSlotAccumulatedLoad accumulates one FlexibleLoad") {
+/*    scenario("SpanSlotAccumulatedLoad accumulates one FlexibleLoad") {
 
       Given("one flexible load")
 
@@ -64,7 +64,7 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
       val flexibleLoad = SpanSlotFlexibleLoad(1, 0, rawFlexibleLoad)
 
-      When("creating a SpanSlotAccumulatedLoad")
+      When("a SpanSlotAccumulatedLoad is created")
 
       val spanSlotAccumulatedLoad = SpanSlotAccumulatedLoad(0, List.empty, Set(flexibleLoad))
 
@@ -89,7 +89,7 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
 
       val flexibleLoads = rawFlexibleLoads.zipWithIndex.map { case (l, idx) =>  SpanSlotFlexibleLoad(idx, idx, l) }
 
-      When("creating a SpanSlotAccumulatedLoad")
+      When("a SpanSlotAccumulatedLoad is created")
 
       val spanSlotAccumulatedLoad = SpanSlotAccumulatedLoad(0, List.empty, flexibleLoads.toSet)
 
@@ -100,6 +100,39 @@ class SpanSlotAccumulatedLoadSpec extends FeatureSpec with GivenWhenThen {
       And("span should be equal to the number of fixed loads")
 
       spanSlotAccumulatedLoad.span should equal(5)
+
+    }*/
+
+    scenario("SpanSlotAccumulatedLoad accumulates fixed and flexible loads") {
+
+      Given("a fixed loads and a flexible load")
+
+      val rawFixedLoads = Vector[Double](1.0, 2.0, 3.0, 4.0, 1.0, 34.0, 50.0, 100000.0)
+
+      val fixedLoad = SpanSlotFixedLoad(0, 0, rawFixedLoads)
+
+      val rawFlexibleLoads = List(
+        Vector[Double](1, 1, 1, 1),
+        Vector[Double](1, 1, 4, 3, 5)
+      )
+
+      val flexibleLoads = List(
+        SpanSlotFlexibleLoad(1, 0, rawFlexibleLoads(0)),
+        SpanSlotFlexibleLoad(2, 4, rawFlexibleLoads(1))
+      )
+
+
+      When("a SpanSlotAccumulatedLoad is created")
+
+      val spanSlotAccumulatedLoad = SpanSlotAccumulatedLoad(0, (fixedLoad :: flexibleLoads).toSet)
+
+      Then("amplitudePerSlot should contain a vector with multiple elements")
+
+      spanSlotAccumulatedLoad.amplitudePerSlot should equal(Vector[Double](2, 3, 4, 5, 2, 35, 54, 100003, 5))
+
+      And("span should be equal to the number of fixed loads")
+
+      spanSlotAccumulatedLoad.span should equal(rawFlexibleLoads.flatten.size)
 
     }
 
