c1e35febb6419b4ba4d344698e09fa11a5935b76
Victor Caballero <victor.caballero@salle.url.edu>
2019-06-12 15:40:12 +0200

    Gui and related main folders deleted

--- src/main/scala/gui/GridGuiActor.scala
+++
@@ -1,9 +0,0 @@
-package gui
-import akka.actor.Actor
-import eventmanager.Events.NewClusters
-
-class GridGuiActor extends Actor {
-  override def receive: Receive = {
-    case NewClusters(clusters) => GridGui.drawClusters(clusters)
-  }
-}
