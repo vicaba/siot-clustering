102118e2c26c92355195ec4996b85ac5b28c4a1f
vicaba <vicaba@me.com>
2019-07-22 19:59:53 +0200

    Bias removed. Overlap ratio calculus changed

--- src/main/scala/test/reschedulermetrics/BiasedAverageDistanceTransformation.scala
+++ src/main/scala/test/reschedulermetrics/BiasedAverageDistanceTransformation.scala
@@ -3,42 +3,39 @@ import test.{Load, Movement, SpanSlotAccumulatedLoad}
 
 class BiasedAverageDistanceTransformation(val bias: Double = 0.50) extends MetricTransformation {
 
-  override def apply(referenceAverage: Double, bestMovement: Movement, temporaryMovement: Movement, preferredSlots: List[Int]): (Double, Double) =
+  override def apply(referenceAverage: Double,
+                     bestMovement: Movement,
+                     temporaryMovement: Movement,
+                     preferredSlots: List[Int]): (Double, Double) =
     (computeAverageDistanceMetric(referenceAverage, temporaryMovement, preferredSlots, bias),
-      computeAverageDistanceMetric(referenceAverage, bestMovement, preferredSlots, bias))
+     computeAverageDistanceMetric(referenceAverage, bestMovement, preferredSlots, bias))
 
   private def computeAverageDistanceMetric(referenceAverage: Double,
-                                   movement: Movement,
-                                   preferedSlots: List[Int],
-                                   bias: Double = 0.50): Double = {
-    val acc          = movement.acc
+                                           movement: Movement,
+                                           preferedSlots: List[Int],
+                                           bias: Double = 0.50): Double = {
+    val accLoad      = movement.acc
     val flexibleLoad = movement.fl
 
-    val actualAverage     = computeAverageAtLoadPosition(acc, flexibleLoad)
-    val slotsWithPriority = computeSlotsWithPriority(flexibleLoad, preferedSlots)
+    val currentAverage                = computeAverageAtLoadPosition(accLoad, flexibleLoad)
+    val slotsWithPriorityOverlapRatio = computeSlotsWithPriorityOverlapRatio(flexibleLoad, preferedSlots)
 
-    val distance = Math.pow(Math.abs(referenceAverage - actualAverage), 1)
-    println(s"average_ref = $referenceAverage, bias = $bias, slotsWithPriority = $slotsWithPriority")
+    val distance = Math.abs(referenceAverage - currentAverage)
+    println(s"average_ref = $referenceAverage, bias = $bias, slotsWithPriority = $slotsWithPriorityOverlapRatio")
 
     // TODO: It seems that removing bias works better
-    distance * (1 - bias * slotsWithPriority)
+    distance * (1 - slotsWithPriorityOverlapRatio)
   }
 
-  private def computeSlotsWithPriority(load: Load, preferedSlots: List[Int]): Double = {
-    val howManySlots = (load.positionInT until (load.positionInT + load.span)).count(p => preferedSlots.contains(p))
-    //println(s"howManySlots = $howManySlots, size = ${preferedSlots.size}")
+  private def computeSlotsWithPriorityOverlapRatio(load: Load, preferredSlots: List[Int]): Double =
+    if (preferredSlots.isEmpty) 0.0
+    else {
+      val numberOfOverlappedSlots =
+        (load.positionInT until (load.positionInT + load.span)).count(p => preferredSlots.contains(p))
+      numberOfOverlappedSlots.toDouble / load.span.toDouble
+    }
 
-    if (preferedSlots.isEmpty) 0.0
-    else howManySlots.toDouble / preferedSlots.size.toDouble
-  }
-
-  private def computeAverageAtLoadPosition(accumulatedLoad: SpanSlotAccumulatedLoad, load: Load): Double = {
-    val fromSlot  = load.positionInT
-    val untilSlot = load.positionInT + load.span
-
-    val average = accumulatedLoad.amplitudePerSlot.slice(fromSlot, untilSlot).sum / load.span
-
-    average
-  }
+  private def computeAverageAtLoadPosition(accumulatedLoad: SpanSlotAccumulatedLoad, load: Load): Double =
+    accumulatedLoad.amplitudePerSlot.slice(load.positionInT, load.positionInT + load.span).sum / load.span
 
 }
