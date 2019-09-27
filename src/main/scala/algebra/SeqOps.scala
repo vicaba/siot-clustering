74b4fd6390817be430801a199d531cb04ddb50be
vicaba <vicaba@me.com>
2019-09-28 00:26:12 +0200

    test package tests in green. Ready to compare with new_test package

--- src/main/scala/algebra/SeqOps.scala
+++ src/main/scala/algebra/SeqOps.scala
@@ -8,40 +8,33 @@ import scala.collection.generic.CanBuildFrom
 
 object SeqOps {
 
-  def sum(s: Seq[Seq[Double]]): Vector[Double] = {
-    val dvs: List[DenseVector[Double]] = s.toList.map { v =>
-      DenseVector(v:_ *)
-    }
-    Type.sumVectors(dvs.tail, dvs.head)
-  }.toScalaVector()
-
-/*  def sum[X: Numeric, S[Y] <: Seq[Y] with SeqLike[Y, S[Y]]]
+  def sum[X: Numeric, S[Y] <: Seq[Y] with SeqLike[Y, S[Y]]]
   (s: Seq[S[X]])
-  (implicit
-  cbf1: CanBuildFrom[S[_], X, S[X]],
-  cbf2: CanBuildFrom[S[_], (X, X), S[(X, X)]]
-  )
+    (implicit
+      cbf1: CanBuildFrom[S[_], X, S[X]],
+      cbf2: CanBuildFrom[S[_], (X, X), S[(X, X)]]
+    )
   : S[X] = {
     val num = implicitly[Numeric[X]]
     import num._
-    s.reduce(_.zip(_)(cbf2).map{ case (a, b) => a + b })
-  }*/
+    s.reduce(_.zip(_)(cbf2).map { case (a, b) => a + b })
+  }
 
   def substract[X: Numeric, S[Y] <: Seq[Y] with SeqLike[Y, S[Y]]]
   (s: Seq[S[X]])
-  (implicit
-   cbf1: CanBuildFrom[S[_], X, S[X]],
-   cbf2: CanBuildFrom[S[_], (X, X), S[(X, X)]]
-  )
+    (implicit
+      cbf1: CanBuildFrom[S[_], X, S[X]],
+      cbf2: CanBuildFrom[S[_], (X, X), S[(X, X)]]
+    )
   : S[X] = {
     val num = implicitly[Numeric[X]]
     import num._
-    s.reduce(_.zip(_)(cbf2).map{ case (a, b) => a - b })
+    s.reduce(_.zip(_)(cbf2).map { case (a, b) => a - b })
   }
 
   def main(args: Array[String]): Unit = {
-    println(sum(Seq(Vector(1), Vector(1))))     // Vector(2)
-    println(sum(Seq(List(2, 3), List(4, 5))))   // List(6, 8)
+    println(sum(Seq(Vector(1), Vector(1)))) // Vector(2)
+    println(sum(Seq(List(2, 3), List(4, 5)))) // List(6, 8)
   }
 
 }
