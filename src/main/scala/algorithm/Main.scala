7b1effc41208e5fb9e1ff8e5772be30e79ed51b9
vicaba <vicaba@me.com>
2019-10-18 17:10:06 +0200

    Nuber of splits changed.

--- src/main/scala/algorithm/Main.scala
+++ src/main/scala/algorithm/Main.scala
@@ -133,7 +133,7 @@ object Main {
     val monteCarlos = for (i <- BigDecimal(Configuration.CrossFold.SubsampleSize.from) to (Max, step = BigDecimal(0.1)))
       yield {
         val subsampleSize = Percentage.of(i / Max)
-        val splits = batchRunSettingsBuilder.points.size / 2
+        val splits = Math.floor((batchRunSettingsBuilder.points.size * subsampleSize.v).toDouble).toInt / 2
         MonteCarlo(splits, subsampleSize)
       }
     val stepsList = CrossFoldValidation.batchRunClusterer(monteCarlos.toList, batchRunSettingsBuilder)
