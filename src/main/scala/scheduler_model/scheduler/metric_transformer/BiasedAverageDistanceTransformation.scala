e14301bf75d9fafa54410b9d8e86565cd1506eaa
Victor Caballero <victor.caballero@salle.url.edu>
2019-11-14 09:49:59 +0100

    Average distance changed to distance sums

--- src/main/scala/scheduler_model/scheduler/metric_transformer/BiasedAverageDistanceTransformation.scala
+++ src/main/scala/scheduler_model/scheduler/metric_transformer/BiasedAverageDistanceTransformation.scala
@@ -7,9 +7,9 @@ import scheduler_model.scheduler.Movement
 class BiasedAverageDistanceTransformation(val bias: Double = 0.50) extends MetricTransformation {
 
   override def apply(referenceAverage: Double,
-    bestMovement: Movement,
-    temporaryMovement: Movement,
-    preferredSlots: List[Int]): MetricTransformationResult =
+                     bestMovement: Movement,
+                     temporaryMovement: Movement,
+                     preferredSlots: List[Int]): MetricTransformationResult =
     MetricTransformationResult(
       computeAverageDistanceMetric(referenceAverage, bestMovement, preferredSlots, bias),
       computeAverageDistanceMetric(referenceAverage, temporaryMovement, preferredSlots, bias)
@@ -18,16 +18,17 @@ class BiasedAverageDistanceTransformation(val bias: Double = 0.50) extends Metri
   override def toString(): String = "BiasedAverageDistanceTransformation"
 
   private def computeAverageDistanceMetric(referenceAverage: Double,
-    movement: Movement,
-    preferredSlots: List[Int],
-    bias: Double = 0.50): Double = {
+                                           movement: Movement,
+                                           preferredSlots: List[Int],
+                                           bias: Double = 0.50): Double = {
     val accLoad      = movement.acc
     val flexibleLoad = movement.fl
 
-    val currentAverage                = computeAverageAtLoadPosition(accLoad, flexibleLoad)
+    //val currentAverage                = computeAverageAtLoadPosition(accLoad, flexibleLoad)
     val slotsWithPriorityOverlapRatio = computeSlotsWithPriorityOverlapRatio(flexibleLoad, preferredSlots)
 
-    val distance = Math.abs(referenceAverage - currentAverage)
+    val distance = computeDistance(referenceAverage, accLoad, flexibleLoad)
+    //val distance = Math.abs(referenceAverage - currentAverage)
     //println(s"average_ref = $referenceAverage, bias = $bias, slotsWithPriority = $slotsWithPriorityOverlapRatio")
 
     // TODO: It seems that removing bias works better
@@ -42,7 +43,18 @@ class BiasedAverageDistanceTransformation(val bias: Double = 0.50) extends Metri
       numberOfOverlappedSlots.toDouble / load.span.toDouble
     }
 
+  private def computeDistance(referenceAverage: Double, accumulatedLoad: AccumulatedLoad, load: Load) = {
+    val slice = accumulatedLoad.amplitudePerSlot.toDenseVector
+      .slice(load.startPositionInTime, load.startPositionInTime + load.span)
+    slice.foldLeft(0.0) {
+      case (accum, elem) =>
+        accum + Math.abs(referenceAverage - elem)
+    }
+  }
+
   private def computeAverageAtLoadPosition(accumulatedLoad: AccumulatedLoad, load: Load): Double =
-    sum(accumulatedLoad.amplitudePerSlot.toDenseVector.slice(load.startPositionInTime, load.startPositionInTime + load.span)) / load.span.toDouble
+    sum(
+      accumulatedLoad.amplitudePerSlot.toDenseVector.slice(load.startPositionInTime,
+                                                           load.startPositionInTime + load.span)) / load.span.toDouble
 
 }
