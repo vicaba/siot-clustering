a9f31dcd3ccd059f8aa7cdb63900bd2bc44fb28f
vcaballero-salle <victor.caballero@salle.url.edu>
2020-04-15 15:36:57 +0200

    GlobalConfig singleton added and used.
    GlobalConfig defaults to SyntheticGlobalConfig.
    GlobalConfig used in ClusterAndAccumulatedLoadTransformer and ClusterRescheduler.

--- src/main/scala/config/GlobalConfig.scala
+++ src/main/scala/config/GlobalConfig.scala
@@ -1,22 +1,30 @@
 package config
 
 import reader.{EgaugeFlexibleLoadsKeys, FlexibleLoadsKeys, SyntheticLoadsFlexibleLoadsKeys}
+import scheduler_model.sequence_split.{SequenceSplitByConsecutiveElements, SequenceSplitByZero, SequenceSplitStrategy}
 
 trait GlobalConfig {
 
   val flexibleLoadsKeys: List[String]
 
+  val sequenceSplitStrategy: SequenceSplitStrategy
+
 }
 
 object GlobalConfig {
+  /**
+    * SyntheticGlobalConfig is de default global configuration for tests
+    */
   var instance: GlobalConfig = SyntheticGlobalConfig()
 }
 
 case class EgaugeGlobalConfig(
-    override val flexibleLoadsKeys: List[String] = EgaugeFlexibleLoadsKeys.flexibleLoads
+    override val flexibleLoadsKeys: List[String] = EgaugeFlexibleLoadsKeys.flexibleLoads,
+    override val sequenceSplitStrategy: SequenceSplitStrategy = SequenceSplitByZero
 ) extends GlobalConfig
 
 case class SyntheticGlobalConfig(
-  override val flexibleLoadsKeys: List[String] = SyntheticLoadsFlexibleLoadsKeys.flexibleLoads
+    override val flexibleLoadsKeys: List[String] = SyntheticLoadsFlexibleLoadsKeys.flexibleLoads,
+    override val sequenceSplitStrategy: SequenceSplitStrategy =
+      SequenceSplitByConsecutiveElements.withConsecutiveValueAsTheHighestCountAndConsecutiveValueBelowAverage
 ) extends GlobalConfig
-
